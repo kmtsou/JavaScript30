136f856e6bb22cd006993a0697db6acfbade9fda
kevin <kmtsou@sbcglobal.net>
2023-12-15 13:38:41 -0800

    .

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -14,3 +14,16 @@ function getVideo() {
         console.log('Oh No!', err);
     })
 }
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+getVideo();
