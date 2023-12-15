167d3d573a792ecaf62c1c566fa148866d20b7eb
kevin <kmtsou@sbcglobal.net>
2023-12-15 13:49:31 -0800

    .

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,4 +26,19 @@ function paintToCanvas() {
     }, 16);
 }
 
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    // link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome guy"/>`
+    strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
